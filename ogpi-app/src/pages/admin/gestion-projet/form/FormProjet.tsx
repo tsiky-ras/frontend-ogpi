72fbb03374d7b360f35cc0bc0f28515a2d131c03
Iandraina <217048206+Iandraina-etech@users.noreply.github.com>
2026-03-30 17:32:04 +0300

    Update FormProjet.tsx

--- ogpi-app/src/pages/admin/gestion-projet/form/FormProjet.tsx
+++ ogpi-app/src/pages/admin/gestion-projet/form/FormProjet.tsx
@@ -165,7 +165,7 @@ const FormProjet: React.FC<FormProjetProps> = ({ show, onClose, onSubmit, projet
     const fetchLeads = async () => {
       setLeadSearchLoading(true);
       try {
-        const results = await leadService.getAll();
+        const results = await leadService.getEvaluating();
         setAllLeads(results || []);
       } catch (err) {
         console.error(err);
